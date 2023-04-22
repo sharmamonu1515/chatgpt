7fd2b6f1e534291cccee441e4e251b57cbfa6f0d
Sharmamonu1515 <sharmamonu1515@gmail.com>
2023-04-22 19:19:55 +0530

    Client post url update

--- client/script.js
+++ client/script.js
@@ -72,7 +72,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv)
 
   // fetch data from server
-  const response = await fetch('http://localhost:5000/', {
+  const response = await fetch('https://chatgpt-szj9.onrender.com/', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
